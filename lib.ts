56431bb2a12833bab62b41024e8dbe6ff53fbcf7
Ivan Dimitrov <ivan@idimitrov.dev>
2023-10-04 13:35:09 +0300

    prepare for ink with lib

---
+++ lib.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env -S bun run
+import xdg from '@folder/xdg'
+import download from 'image-downloader'
+import fs from 'node:fs'
+import dayjs from 'dayjs'
+
+const userDirs = xdg.userdirs.expand().dirs()
+const picturesDir = userDirs.XDG_PICTURES_DIR_DIR
+const oldPicturesDir = `${picturesDir}/bg.old/`
+const picPath = `${picturesDir}/bg.png`
+
+export const bing = async () => {
+    const bingUrl = "https://www.bing.com/HPImageArchive.aspx/?format=js&idx=0&n=1&mkt=en-US"
+    const json = await fetch(bingUrl).then(res => res.json())
+    const image = json["images"][0]["url"]
+    return "https://bing.com" + image
+}
+
+if (!fs.existsSync(oldPicturesDir)) {
+    fs.mkdirSync(oldPicturesDir)
+}
+fs.copyFileSync(picPath, `${oldPicturesDir}${dayjs().format("DD-MM-YYYY")}.png`)
+
+export const dl = async (url: string) => {
+    const options = {
+        url: await bing(),
+        dest: `${picPath}`,
+    };
+    // @ts-ignore
+    download.image(options)
+        .then(({ filename }) => {
+            console.log('Saved to', filename);
+        })
+        .catch((err) => console.error(err));
+}
+
